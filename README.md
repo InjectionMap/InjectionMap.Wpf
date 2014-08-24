f92216b310df1de15ef6844578cff31a6ccc585b
chriswalpen <c.walpen@gmx.net>
2014-08-24 22:23:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+![wickedflame injectionmap](assets/wickedflame injectionmap - black.png)
+
+# InjectionMap.Wpf
+------------------------------
+InjectionMap is a small extension to InjectionMap for WPF Applications. 
+This extension allows viewmodels to be injected into the DataContext of a View (FrameworkElement).
+
+# Examples
+------------------------------
+1. Create a View (Usercontrol/Window/...)  
+```csharp
+<Window x:Class="MainWindow">
+	...
+</Window>
+```
+2. Create a ViewModel (A class that will be added to the DataContext)
+```csharp
+namespace ViewModels
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		...
+	}
+}
+```
+3. Bind the ViewModel to the View
+```csharp
+<Window x:Class="MainWindow"
+        xmlns:resolver="clr-namespace:InjectionMap;
+						assembly=personalplaner.common"
+        xmlns:vm="clr-namespace:ViewModels"
+        resolver:DataContextResolver.Resolve="{x:Type vm:ViewModel}">
+...
+</Window>
+```
+
+For ViewModels that just use the default constructor, the ViewModel does not have to be mapped to InjectionMap first.
+
+## Installation
+------------------------------
+InjectionMap can be installed from [NuGet](http://docs.nuget.org/docs/start-here/installing-nuget) through the package manager console:  
+
+    PM > Install-Package InjectionMap.Wpf
+
+## Bugs, issues and features
+------------------------------
+Bugs, issues or feature wishes can submitted on the [issues](https://github.com/InjectionMap/InjectionMap.Wpf/issues) page or feel free to fork the project and send a pull request.
+
+
+InjectionMap is developed by [wickedflame](http://wicked-flame.blogspot.ch/) under the [Ms-PL License](License.txt).
---
+++ src/InjectionMap.Wpf/DataContextResolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace InjectionMap
+{
+    public class DataContextResolver
+    {
+        #region Resolve
+
+        /// <summary>
+        /// Resolve Attached Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty ResolveProperty = DependencyProperty.RegisterAttached(
+            "Resolve",
+            typeof(Type),
+            typeof(DataContextResolver),
+            new FrameworkPropertyMetadata((Type)null, new PropertyChangedCallback(OnResolveChanged)));
+
+        /// <summary>
+        /// Gets the Resolve property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public static Type GetResolve(DependencyObject d)
+        {
+            return (Type)d.GetValue(ResolveProperty);
+        }
+
+        /// <summary>
+        /// Sets the Resolve property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public static void SetResolve(DependencyObject d, Type value)
+        {
+            d.SetValue(ResolveProperty, value);
+        }
+
+        /// <summary>
+        /// Handles changes to the Resolve property.
+        /// </summary>
+        private static void OnResolveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var element = d as FrameworkElement;
+            if (element == null)
+            {
+                throw new InvalidOperationException("Objects can only be injected into the DataContext FrameworkElements");
+            }
+
+            using (var resolver = new InjectionResolver())
+            {
+                element.DataContext = resolver.Resolve(e.NewValue as Type);
+            }
+        }
+
+        #endregion
+    }
+}
