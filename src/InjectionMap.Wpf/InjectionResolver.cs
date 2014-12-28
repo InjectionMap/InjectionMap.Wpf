2761ce492feb1af89892b8efd869fd96c4829d2f
chriswalpen <c.walpen@gmx.net>
2014-12-28 22:25:23 +0100

    Resolve from custom context

--- src/InjectionMap.Wpf/InjectionResolver.cs
+++ src/InjectionMap.Wpf/InjectionResolver.cs
@@ -56,12 +56,47 @@ namespace InjectionMap.Wpf
                 throw new InvalidOperationException("Objects can only be injected into the DataContext FrameworkElements");
             }
 
-            using (var resolver = new InjectionMap.InjectionResolver())
+            var contextName = GetContextName(d);
+            var context = new MappingContext(contextName);
+            using (var resolver = new InjectionMap.InjectionResolver(context))
             {
                 Logger.Write(string.Format("InjectionMap.Wpf - Inject {0} to DataContext of {1}", e.NewValue, d.GetType()), LogLevel.Info, "InjectionMap.Wpf.InjectionResolver", "InjectionResolver", DateTime.Now);
                 element.DataContext = resolver.Resolve(e.NewValue as Type);
             }
         }
+
+        #endregion
+
+        #region Context Name
+
+        /// <summary>
+        /// Gets the name of the Context the map is stored in
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetContextName(DependencyObject obj)
+        {
+            return (string)obj.GetValue(ContextNameProperty);
+        }
+
+        /// <summary>
+        /// Sets the name of the Context the map is stored in
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetContextName(DependencyObject obj, string value)
+        {
+            obj.SetValue(ContextNameProperty, value);
+        }
+
+        /// <summary>
+        /// Gets the name of the Context the map is stored in
+        /// </summary>
+        public static readonly DependencyProperty ContextNameProperty = DependencyProperty.RegisterAttached(
+            "ContextName", 
+            typeof(string), 
+            typeof(InjectionResolver), 
+            new PropertyMetadata(null));
 
         #endregion
     }
